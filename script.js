69847a45eb2c662eca687e04a54c200385e4278a
dipanshuu <dipanshusahu447@gmail.com>
2025-04-03 16:49:11 +0530

    Recent search feature succesfully added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var searchBar = document.querySelector('.searchbar input');
 var searchButton = document.querySelector('.searchbar img:nth-of-type(2)');
-var recentSearch = document.querySelector('.recent-search');
+var recentSearch = document.querySelector('#recent-search');
+
+window.onload = displayRecentSearches;
 
 async function getWeatherData(city) {
     const url = `http://api.openweathermap.org/data/2.5/weather?appid=fdc55ecc7901d54bf227ce26bde77c52&units=metric&q=${city}`;
@@ -12,6 +14,8 @@ async function getWeatherData(city) {
         console.log("Humidity : "+ data.main.humidity);
         console.log("Wind Speed : "+ (data.wind.speed * 3.6).toFixed(2));
         console.log("Icon : "+ data.weather[0].icon);
+        storeSearch(city);
+        displayRecentSearches();
         updateWeatherData(data);
         console.log(data);
     } catch (error) {
@@ -47,6 +51,41 @@ searchButton.addEventListener('click',() => {
     }
 })
 
-function storeSearch(city) {
+function displayRecentSearches() {
+    let searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
+    let imgsrc;
+    if(searches.length === 0){
+        searches = ['Mumbai','Banglore','New york','Spain'];
+        imgsrc = 'https://img.icons8.com/?size=100&id=73828&format=png&color=9C9C9C';
+    }else {
+        imgsrc = 'https://img.icons8.com/?size=100&id=82767&format=png&color=9C9C9C';
+    }
+    
+    recentSearch.innerHTML = "";
 
+    searches.forEach((city) => {
+        let btn = document.createElement('button');
+        let img = document.createElement('img');
+        img.src = imgsrc;
+        img.alt = 'clock';
+        btn.onclick = () => getWeatherData(city);
+        btn.appendChild(img);
+        btn.append(city);
+        recentSearch.appendChild(btn);
+    })
+}
+
+function storeSearch(city) {
+    let btns = Array.from(recentSearch.childNodes);
+    let searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
+
+    if(!searches.includes(city)){
+        if(searches.length >= 4){
+            recentSearch.removeChild(btns.pop());
+            searches.pop();
+        }
+        searches.unshift(city);
+    }
+
+    localStorage.setItem('recentSearches',JSON.stringify(searches));
 }
