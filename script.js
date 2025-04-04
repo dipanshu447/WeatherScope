d77ccce72ebb0b87bb4a6761672f28929db7f65d
dipanshuu <dipanshusahu447@gmail.com>
2025-04-04 15:11:42 +0530

    Ui error message feature added sucessfully

--- script.js
+++ script.js
@@ -4,26 +4,45 @@ var recentSearch = document.querySelector('#recent-search');
 
 window.onload = displayRecentSearches;
 
+function showError(error) {
+    let errorBox = document.createElement('div');
+    errorBox.classList = 'error-message';
+    errorBox.innerHTML = `<div class="flex gap-2 item-center justify-center">
+            <img src="https://img.icons8.com/?size=100&id=7JSM2OfYelRz&format=png&color=ffffff" alt="error-image">
+            ${error}
+        </div>
+        <img src="https://img.icons8.com/?size=100&id=6483&format=png&color=ffffff" alt="cross">`;
+    document.body.appendChild(errorBox);
+    errorBox.querySelectorAll('img')[1].addEventListener('click', () => errorBox.remove());
+    setTimeout(() => errorBox.remove(), 7000);
+}
+
 async function getWeatherData(city) {
     const url = `http://api.openweathermap.org/data/2.5/weather?appid=fdc55ecc7901d54bf227ce26bde77c52&units=metric&q=${city}`;
     try {
         let response = await fetch(url);
         let data = await response.json();
-        console.log('Temprature : '+ Math.round(data.main.temp));
-        console.log("Description : "+ data.weather[0].main);
-        console.log("Humidity : "+ data.main.humidity);
-        console.log("Wind Speed : "+ (data.wind.speed * 3.6).toFixed(2));
-        console.log("Icon : "+ data.weather[0].icon);
+
+        if (data.cod === "404") {
+            showError(data.message);
+            return;
+        }
+        console.log('Temprature : ' + Math.round(data.main.temp));
+        console.log("Description : " + data.weather[0].main);
+        console.log("Humidity : " + data.main.humidity);
+        console.log("Wind Speed : " + (data.wind.speed * 3.6).toFixed(2));
+        console.log("Icon : " + data.weather[0].icon);
         storeSearch(city);
         displayRecentSearches();
         updateWeatherData(data);
         console.log(data);
     } catch (error) {
+        showError("Failed to fetch data. Check your internet connection.");
         console.log(error);
     }
 }
 
-var updateWeatherData = async (data) =>  {
+var updateWeatherData = async (data) => {
     document.querySelector('.status h1').innerHTML = `${Math.round(data.main.temp)}<sup>°C</sup>`;
     document.querySelector('.status p').innerText = data.weather[0].main;
     document.querySelector('.humidity h3').innerHTML = `${data.main.humidity}%`;
@@ -33,20 +52,20 @@ var updateWeatherData = async (data) =>  {
     document.querySelectorAll('.disnone').forEach(e => e.classList.remove('disnone'));
 }
 
-function searchWeather(){
-    if(searchBar.value.length >= 2){
+function searchWeather() {
+    if (searchBar.value.length >= 2) {
         getWeatherData(searchBar.value);
     }
 }
 
 searchBar.addEventListener('keydown', (e) => {
-    if(e.key == 'Enter'){
+    if (e.key == 'Enter') {
         searchWeather();
     }
 })
 
-searchButton.addEventListener('click',() => {
-    if(searchBar.value.length >= 2){
+searchButton.addEventListener('click', () => {
+    if (searchBar.value.length >= 2) {
         searchWeather();
     }
 })
@@ -54,13 +73,13 @@ searchButton.addEventListener('click',() => {
 function displayRecentSearches() {
     let searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
     let imgsrc;
-    if(searches.length === 0){
-        searches = ['Mumbai','Banglore','New york','Spain'];
+    if (searches.length === 0) {
+        searches = ['Mumbai', 'Banglore', 'New york', 'Spain'];
         imgsrc = 'https://img.icons8.com/?size=100&id=73828&format=png&color=9C9C9C';
-    }else {
+    } else {
         imgsrc = 'https://img.icons8.com/?size=100&id=82767&format=png&color=9C9C9C';
     }
-    
+
     recentSearch.innerHTML = "";
 
     searches.forEach((city) => {
@@ -79,13 +98,13 @@ function storeSearch(city) {
     let btns = Array.from(recentSearch.childNodes);
     let searches = JSON.parse(localStorage.getItem('recentSearches')) || [];
 
-    if(!searches.includes(city)){
-        if(searches.length >= 4){
+    if (!searches.includes(city)) {
+        if (searches.length >= 4) {
             recentSearch.removeChild(btns.pop());
             searches.pop();
         }
         searches.unshift(city);
     }
 
-    localStorage.setItem('recentSearches',JSON.stringify(searches));
+    localStorage.setItem('recentSearches', JSON.stringify(searches));
 }
